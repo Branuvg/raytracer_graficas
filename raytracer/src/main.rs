b0fbce90f5f7b54483d091f7ec2f41135c7978ee
Gabriel Bran <bra23590@uvg.edu.gt>
2025-08-16 21:56:58 -0600

    materials without depth

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -9,7 +9,7 @@ mod ray_intersect;
 mod sphere;
 
 use framebuffer::Framebuffer;
-use ray_intersect::RayIntersect;
+use ray_intersect::{RayIntersect, Intersect, Material};
 use sphere::Sphere;
 
 pub fn cast_ray(
@@ -17,12 +17,20 @@ pub fn cast_ray(
     ray_direction: &Vector3,
     objects: &[Sphere],
 ) -> Color {
+    let mut intersect = Intersect::empty();
+
     for object in objects {
-        if object.ray_intersect(ray_origin, ray_direction) {
-            return Color::new(157, 165, 189, 255);
+        let tmp = object.ray_intersect(ray_origin, ray_direction);
+        if tmp.is_intersecting {
+            intersect = tmp;
         }
     }
-    Color::new(4, 12, 36, 255)
+
+    if !intersect.is_intersecting {
+        return Color::new(4, 12, 36, 255);
+    }
+
+    intersect.material.diffuse
 }
 
 pub fn render(framebuffer: &mut Framebuffer, objects: &[Sphere]) {
@@ -42,7 +50,6 @@ pub fn render(framebuffer: &mut Framebuffer, objects: &[Sphere]) {
             let camera_y = screen_y * perspective_scale;
 
             let ray_direction = Vector3::new(camera_x, camera_y, -1.0).normalized();
-            
             let ray_origin = Vector3::new(0.0, 0.0, 0.0);
 
             let pixel_color = cast_ray(&ray_origin, &ray_direction, objects);
@@ -57,26 +64,42 @@ fn main() {
     let window_width = 1300;
     let window_height = 900;
 
-    let (mut rl, thread) = raylib::init()
+    let (mut window, raylib_thread) = raylib::init()
         .size(window_width, window_height)
-        .title("Raytracer en Rust")
+        .title("Raytracer Class")
         .log_level(TraceLogLevel::LOG_WARNING)
         .build();
 
     let mut framebuffer = Framebuffer::new(window_width as i32, window_height as i32);
 
-    framebuffer.set_background_color(Color::new(4, 12, 36, 255));
+    let rubber = Material {
+        diffuse: Color::new(80, 0, 0, 255),
+    };
 
-    let objects = [Sphere {
-        center: Vector3::new(0.0, 0.0, -5.0),
+    let ivory = Material {
+        diffuse: Color::new(100, 100, 80, 255),
+    };
+
+    //framebuffer.set_background_color(Color::new(4, 12, 36, 255));
+
+    let objects = [
+        Sphere {
+        center: Vector3::new(1.0, 0.0, -4.0),
         radius: 1.0,
-    }];
+        material: ivory,
+    },
+    Sphere {
+        center: Vector3::new(2.0, 0.0, -5.0),
+        radius: 1.0,
+        material: rubber,
+    },
+    ];
 
-    while !rl.window_should_close() {
+    while !window.window_should_close() {
         framebuffer.clear();
 
         render(&mut framebuffer, &objects);
 
-        framebuffer.swap_buffers(&mut rl, &thread);
+        framebuffer.swap_buffers(&mut window, &raylib_thread);
     }
 }
--- raytracer/src/sphere.rs
+++ raytracer/src/sphere.rs
@@ -1,21 +1,31 @@
 // sphere.rs
 use raylib::prelude::*;
-use crate::ray_intersect::RayIntersect;
+use crate::ray_intersect::{RayIntersect, Intersect, Material};
 
 pub struct Sphere {
     pub center: Vector3,
     pub radius: f32,
+    pub material: Material,
 }
 
 impl RayIntersect for Sphere {
-    fn ray_intersect(&self, ray_origin: &Vector3, ray_direction: &Vector3) -> bool {
+    fn ray_intersect(&self, ray_origin: &Vector3, ray_direction: &Vector3) -> Intersect {
         
         let oc = *ray_origin - self.center;
+
         let a = ray_direction.dot(*ray_direction);
         let b = 2.0 * oc.dot(*ray_direction);
         let c = oc.dot(oc) - self.radius * self.radius;
+
         let discriminant = b * b - 4.0 * a * c;
 
-        discriminant >= 0.0
+        if discriminant > 0.0 {
+            let t = (-b - discriminant.sqrt()) / (2.0 * a);
+            if t > 0.0 {
+                return Intersect::new(self.material);
+            }
+        }
+
+        Intersect::empty()
     }
 }
